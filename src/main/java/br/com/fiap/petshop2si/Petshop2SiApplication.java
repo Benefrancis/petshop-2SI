304e64f08a253768472ce2780e91ace8723fdc0f
Benefrancis <benefrancis@gmail.com>
2023-05-29 20:24:43 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/petshop2si/Petshop2SiApplication.java
@@ -0,0 +1,15 @@
+package br.com.fiap.petshop2si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petshop2SiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Petshop2SiApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/br/com/fiap/petshop2si/Petshop2SiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.petshop2si;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petshop2SiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
